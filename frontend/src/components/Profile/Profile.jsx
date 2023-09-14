f84fd6f2cdbc2375f5c8e1aacf57fee154b87d9d
Emilis Druteika <101561666+emidru1@users.noreply.github.com>
2023-09-14 20:08:37 +0300

    Restructure function format working login mockup

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,12 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-const Profile = () => {
+export default function Profile() {
     return (
         <div className='profile'>
             <h1>Profile</h1>
         </div>
     );
-};
-
-export default Profile;
+}
--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -1,13 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-const SignUp = () => {
+export default function SignUp() {
     return (
         <div className='signup'>
             <h1>Sign Up</h1>
         </div>
     );
-
-};
-
-export default SignUp;
+}
