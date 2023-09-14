bef7ec7328d78efa144a7ccd814340ec23dbb6b2
Emilis Druteika <101561666+emidru1@users.noreply.github.com>
2023-09-14 19:33:08 +0300

    Login form and basic handling attempt

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,12 +1,52 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, {useState} from "react";
+import PropTypes from "prop-types";
+import "./Login.css";
 
-const Login = () => {
+async function loginUser(credentials) {
+    return fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+    .then(data => data.json())
+}
+
+const Login = ({ setToken }) => {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
     return (
-        <div className='login'>
-            <h1>Login</h1>
+        <div className="login-form">
+            <form onSubmit={handleSubmit}>
+            <label>
+                <p>Email</p>
+                <input type="email" onChange={e => setUsername(e.target.value)}/>
+            </label>
+            <label>
+                <p>Password</p>
+                <input type="password" onChange={e => setPassword(e.target.value)}/>
+            </label>
+            <div className="login-submit">
+                <button type="submit">Submit</button>
+            </div>
+        </form>
         </div>
     );
 };
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+  };
 
 export default Login;
